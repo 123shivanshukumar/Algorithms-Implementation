e54801d0833ea4a478333a752d4187b92c0d1ab0
SHIVANSHU KUMAR <143614626+123shivanshukumar@users.noreply.github.com>
2025-03-10 20:08:08 +0530

    Update graph.hpp

--- graph.hpp
+++ graph.hpp
@@ -1,10 +1,10 @@
 /*
     all algos -- adjacency list representation
 */
- // can change to custom types
+
 #include<vector>
 #include<iostream>
-typedef int elt;
+typedef int elt;  // can change to custom types
 
 class Graph{
     std::vector<std::vector<elt>>adj;
@@ -12,10 +12,10 @@ class Graph{
     friend std::istream& operator>>(std::istream&,Graph&);
     friend std::ostream& operator<<(std::ostream&,Graph&);
 
-    void bfs(std::vector<int>&);
+    std::vector<int> bfs();
     bool isBipartite();
-    void dfs(std::vector<int>&);
-    std::vector<int>& topo_sort();
+    std::vector<int> dfs();
+    std::vector<int> topo_sort();
     bool isCyclic_undir();
     bool isCyclic_dir();
     void print(std::vector<int>&);
@@ -23,5 +23,28 @@ class Graph{
     // s;
     // kosaraju, dijkstra, bellman ford, floyd warshall, more algos in clrs 
 };
+std::vector<int> bfs(){
+    int n = adj.size();
+    std::vector<int> traversal;
+    std::vector<bool> visited(n,false);
+    std::queue<int> bfs;
+    
+    bfs.push(0);
+    visited[0] = true;
+    
+    int v;
+    while(!bfs.empty()){
+        v = bfs.front();
+        bfs.pop();
+        traversal.push_back(v);
+        for(auto u : adj[v]){
+            if(!visited[u]){
+                bfs.push(u);
+                visited[u] = true;
+            }
+        }
+    }
+    return traversal;
+}
 
 
